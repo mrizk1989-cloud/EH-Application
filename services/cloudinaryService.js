a3874b69a6dd62be987f3445ab88589a18d513f0
mrizk1989-cloud <m.rizk1989@gmail.com>
2026-05-04 07:12:53 +0300

    adding 2 bath for upload dev and dep

--- services/cloudinaryService.js
+++ services/cloudinaryService.js
@@ -1,19 +1,26 @@
 const cloudinary = require('../config/cloudinary');
 const streamifier = require('streamifier');
 
+function getUploadFolder() {
+    if (process.env.NODE_ENV === 'production') {
+        return process.env.CLOUDINARY_FOLDER_PROD || "requests";
+    }
+
+    return process.env.CLOUDINARY_FOLDER_DEV || "requests_dev";
+}
+
 /**
  * Upload file buffer to Cloudinary
- * @param {Buffer} fileBuffer
- * @param {String} folder
- * @returns {Promise<Object>}
  */
-function uploadToCloudinary(fileBuffer, folder = "requests") {
+function uploadToCloudinary(fileBuffer) {
     return new Promise((resolve, reject) => {
+
+        const folder = getUploadFolder();
 
         const stream = cloudinary.uploader.upload_stream(
             {
                 folder,
-                resource_type: "auto" // supports PDF, images, office files
+                resource_type: "auto"
             },
             (error, result) => {
                 if (error) return reject(error);
